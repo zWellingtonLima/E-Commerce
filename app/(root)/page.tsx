d3212841176db92383a76e149182d014941993bc
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-07-03 20:45:39 +0100

    Modal.tsx added and dialog.tsx from shadcn-ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
+"use client"
+
+import Modal from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
+      <Modal isOpen onClose={() => {}} title="Test" description="Test Desc" >
+        Children
+      </Modal>
     </div>
   );
 };
