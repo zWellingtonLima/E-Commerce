a48366679577060a16914fffb034d3740c4df93c
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-07-05 08:15:31 +0100

    StoreModal created, ModalProvider also added and used in App layout.tsx rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ["latin"] });
+import { ModalProvider } from "@/providers/modal-provider";
+
+import "./globals.css";
 
 export const metadata = {
   title: "Admin Dashboard",
@@ -17,7 +17,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
--- hooks/use-store-modal.tsx
+++ hooks/use-store-modal.tsx
@@ -1,9 +1,13 @@
 import { create } from "zustand";
 
-interface useStoreModalStoree {
+interface useStoreModalStore {
   isOpen: boolean;
-  onOpen: () => {};
-  onClose: () => {};
+  onOpen: () => void;
+  onClose: () => void;
 }
 
-
+export const useStoreModal = create<useStoreModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
