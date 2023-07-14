072f65334916a869ba70b82a089f4495f5e4aba0
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-07-14 08:00:01 +0100

    Added control and store switcher at user click on stores

--- components/ui/store-switcher.tsx
+++ components/ui/store-switcher.tsx
@@ -1,9 +1,11 @@
-"use client"
+"use client";
 
 import { Store } from "@prisma/client";
 
 import { PopoverTrigger } from "@/components/ui/popover";
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<
   typeof PopoverTrigger
@@ -18,6 +20,25 @@ export default function StoreSwitcher({
   items = [],
 }: StoreSwitcherProps) {
   const storeModal = useStoreModal();
+  const params = useParams();
+  const router = useRouter();
+
+
+  const formattedItems = items.map((item) => ({
+    label: item.name,
+    value: item.id,
+  }));
+
+  const currentStore = formattedItems.find(
+    (item) => item.value === params.storeId
+  );
+
+  const [open, setOpen] = useState(false)
+
+  const onStoreSelect = (store: { value: string, label: string }) => {
+    setOpen(false)
+    router.push(`/${store.value}`)
+  }  
 
   return <div>Store Switcher</div>;
 }
