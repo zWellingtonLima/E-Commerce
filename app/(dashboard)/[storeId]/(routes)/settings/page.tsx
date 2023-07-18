69de58df71a76fa44316afca330e95f56084ad59
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-07-18 08:11:54 +0100

    Settings folder created

---
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { FC } from "react";
+
+import prismadb from "@/lib/prismadb";
+
+interface SettingsPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const SettingsPage: FC<SettingsPageProps> = async ({ params }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-col">
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        {/* <SettingsForm /> */}
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
