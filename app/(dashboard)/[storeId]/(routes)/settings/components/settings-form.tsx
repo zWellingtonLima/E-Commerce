3a95ce65a6da8c1927b2e4831fcbabc1fbb0f34d
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-07-18 19:37:51 +0100

    Feat: onDelete function added to actually delete the store

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -22,6 +22,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import AlertModal from "@/components/modals/alert-modal";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -59,8 +60,30 @@ const SettingsForm: FC<SettingsFormProps> = ({ initialData }) => {
     }
   };
 
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(`/api/stores/${params.storeId}`)
+      router.refresh()
+      router.push('/')
+      toast.success("Store deleted.")
+
+    } catch (error) {
+      toast.error("Make sure you remove all products and categories first.")
+
+    } finally {
+      setLoading(false) 
+    }
+  }
+
   return (
     <>
+      <AlertModal 
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
         <Button
