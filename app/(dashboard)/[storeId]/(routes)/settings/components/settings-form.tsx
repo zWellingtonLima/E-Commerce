ea6d004e9d49d679901c73caeff24d1a2a56763e
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-07-18 16:47:27 +0100

    Feat(lib): form inside settings-form initiated; used useForm and Zod dependencies

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,18 +1,32 @@
 "use client";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
 import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { Trash } from "lucide-react";
 
 import Heading from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
-import { Trash } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
 const SettingsForm: FC<SettingsFormProps> = ({ initialData }) => {
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
+  });
+
   return (
     <>
       <div className="flex items-center justify-between">
