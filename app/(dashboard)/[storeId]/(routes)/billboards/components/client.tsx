e83d3074322618842e4720a675c89711e417205a
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-07-21 15:50:52 +0100

    Feat: created interface to BillboardClient to receive the data prop and used the data.length to remove the hard coded (0) from the Heading title

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -2,23 +2,32 @@
 
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { Billboard } from "@prisma/client";
+import { FC } from "react";
 
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
-const BillboardClient = () => {
+interface BillboardClientProps {
+  data: Billboard[];
+}
+
+const BillboardClient: FC<BillboardClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
 
   return (
     <>
-      <div className='flex items-center justify-between'>
-        <Heading 
-          title="Billboards (0)"
-          description="Manage billboards for your store"/>
-        <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
-          <Plus className='mr-2 h-4 w-4'/>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
           Add New
         </Button>
       </div>
