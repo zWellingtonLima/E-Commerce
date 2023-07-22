86998fe39f4517e06288b9aa54298dd8ad7237f1
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-07-22 16:22:35 +0100

    Feat: added dropdownMenu from shadcn and used it at actions column

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,5 +1,50 @@
-const CellAction = () => {
-  return <div>CellAction</div>
+"use client";
+
+import { FC } from "react";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+
+import { BillboardColumn } from "./columns";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+
+interface CellActionProps {
+  data: BillboardColumn;
 }
 
-export default CellAction
+const CellAction: FC<CellActionProps> = ({ data }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="w-4 h-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>
+          Actions
+        </DropdownMenuLabel>
+        <DropdownMenuItem>
+          <Copy className='w-4 h-4 mr-2'/>
+          Copy Id
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Edit className='w-4 h-4 mr-2'/>
+          Update
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Trash className='w-4 h-4 mr-2'/>
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default CellAction;
