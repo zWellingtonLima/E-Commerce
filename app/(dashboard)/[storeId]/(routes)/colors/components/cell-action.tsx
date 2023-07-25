1c4cca69874a02182d42615fc1991126561e0dfa
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-07-25 16:31:11 +0100

    Fix: fixed the strings mismatches between folder and file names

--- app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
@@ -5,7 +5,7 @@ import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { toast } from "react-hot-toast";
 
-import { SizeColumn } from "./columns";
+import { ColorColumn } from "./columns";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -18,7 +18,7 @@ import axios from "axios";
 import AlertModal from "@/components/modals/alert-modal";
 
 interface CellActionProps {
-  data: SizeColumn;
+  data: ColorColumn;
 }
 
 const CellAction: FC<CellActionProps> = ({ data }) => {
@@ -27,7 +27,7 @@ const CellAction: FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Size ID copied to the clipboard.");
+    toast.success("Color ID copied to the clipboard.");
   };
 
   const [loading, setLoading] = useState(false);
@@ -36,12 +36,12 @@ const CellAction: FC<CellActionProps> = ({ data }) => {
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/colors/${data.id}`);
       router.refresh();
-      toast.success("Size deleted.");
+      toast.success("Color deleted.");
     } catch (error) {
       toast.error(
-        "Make sure you removed all products using this size first."
+        "Make sure you removed all products using this color first."
       );
     } finally {
       setLoading(false);
@@ -71,7 +71,7 @@ const CellAction: FC<CellActionProps> = ({ data }) => {
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/sizes/${data.id}`)
+              router.push(`/${params.storeId}/colors/${data.id}`)
             }
           >
             <Edit className="w-4 h-4 mr-2" />
