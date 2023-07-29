0381d29d4bee619b49b49733644325733292d9ee
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-07-29 21:29:07 +0100

    Feat: .

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -5,12 +5,13 @@ import { useEffect } from "react";
 import { useStoreModal } from "@/hooks/use-store-modal";
 
 const SetupPage = () => {
+  const onClose = useStoreModal();
   const onOpen = useStoreModal((state) => state.onOpen);
   const isOpen = useStoreModal((state) => state.isOpen);
- 
+
   useEffect(() => {
     if (!isOpen) {
-      onOpen()
+      onOpen();
     }
   }, [isOpen, onOpen]);
 
