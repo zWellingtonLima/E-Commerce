e13326c88292abfa4825271ac0a7343af416b0f5
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-10-17 00:16:44 +0100

    Feat: adding content to dashboard page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,67 @@
-const DashboardPage = () => {
-  return <div>DashboardPage</div>
+import { CreditCard, DollarSign, Package } from "lucide-react";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
+
+interface DashboardPageProps {
+  params: { storeId: string };
 }
 
-export default DashboardPage
+const DashboardPage = async ({ params }: DashboardPageProps) => {
+  // const totalRevenue = await 
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-6 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
+              <CardTitle className="text-sm font-medium">
+                Total Revenue
+              </CardTitle>
+              <DollarSign className="h-4 w-4 text-muted-foreground" />
+              <CardContent>
+                <div className='text-2xl font-bold'>
+                  {formatter.format(100)}
+                </div>
+              </CardContent>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
+              <CardTitle className="text-sm font-medium">
+                Sales
+              </CardTitle>
+              <CreditCard className="h-4 w-4 text-muted-foreground" />
+              <CardContent>
+                <div className='text-2xl font-bold'>
+                  +25
+                </div>
+              </CardContent>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
+              <CardTitle className="text-sm font-medium">
+                Products In Stock
+              </CardTitle>
+              <Package className="h-4 w-4 text-muted-foreground" />
+              <CardContent>
+                <div className='text-2xl font-bold'>
+                  12
+                </div>
+              </CardContent>
+            </CardHeader>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -24,5 +24,45 @@ export async function POST(req: Request) {
   const session = event.data.object as Stripe.Checkout.Session;
   const address = session?.customer_details?.address;
 
-  // const addressComponents 
+  const addressComponents = [
+    address?.line1,
+    address?.line2,
+    address?.city,
+    address?.state,
+    address?.postal_code,
+    address?.country,
+  ];
+
+  const addressString = addressComponents.filter((c) => c !== null).join(", ");
+
+  if (event.type === "checkout.session.completed") {
+    const order = await prismadb.order.update({
+      where: {
+        id: session?.metadata?.orderId,
+      },
+      data: {
+        isPaid: true,
+        address: addressString,
+        phone: session?.customer_details?.phone || "",
+      },
+      include: {
+        orderItems: true,
+      },
+    });
+
+    const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+    await prismadb.product.updateMany({
+      where: {
+        id: {
+          in: [...productIds],
+        },
+      },
+      data: {
+        isArchived: true,
+      },
+    });
+  }
+
+  return new NextResponse(null, { status: 200 });
 }
